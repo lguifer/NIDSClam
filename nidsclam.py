645eec8759151d280d722629968b46b4949a499a
Lázaro J.Guillén Fernández <82507283+lguifer@users.noreply.github.com>
2024-12-01 13:00:53 +0100

    Update nidsclam.py

--- nidsclam.py
+++ nidsclam.py
@@ -1,15 +1,18 @@
-import os
+  GNU nano 6.2                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               nidsclam.py                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         import os
 import socket
 import json
 import subprocess
 import argparse
 import logging
 import re
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timezone
 
 # Define the log file path as a global variable
-LOG_FILE_PATH = "/var/log/virus-scanner/virus-scanner.log"
+LOG_FILE_PATH = "/var/log/nidsclam/nidsclam.log"
+YARA_RULES_PATH = "/home/kali/scripts/yara/all_rules.yar"
 hostname = socket.gethostname()
+THREADS = 1
 
 # Ensure the log directory exists
 log_dir = os.path.dirname(LOG_FILE_PATH)
@@ -23,7 +26,7 @@ if not os.path.exists(LOG_FILE_PATH):
 # Set up logging configuration
 logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s - NIDS - %(message)s",
+        format="%(asctime)s NAME_SERVER NIDS[1]: %(message)s",
         handlers=[
                 logging.FileHandler(LOG_FILE_PATH),
                 logging.StreamHandler()  # Also log to the console for visibility
@@ -40,12 +43,15 @@ parser.add_argument("--suricata-log-file", help="Path to the Suricata log file")
 parser.add_argument("--zeek-log-file", help="Path to the Zeek log file")
 parser.add_argument("--recursive", action="store_true", help="Recursively search in filestore-path")
 parser.add_argument("--file-or-dir", help="File or directory to scan directly if no log file is provided")
+parser.add_argument("--threads", help="Number of threads to use analyzing")
 args = parser.parse_args()
 
 # Set default path for date config file if not provided
 if not args.date_config_path:
         args.date_config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "last_processed_date.txt")
 
+if args.threads:
+    THREADS = int(args.threads)
 # Function to read the last processed date
 def read_last_processed_date():
         if os.path.exists(args.date_config_path):
@@ -81,16 +87,23 @@ def process_logs():
 
 # Direct file or directory processing
 def process_direct_file_or_dir(file_or_dir):
-        if os.path.isfile(file_or_dir):
-                process_event(None, "N/A", "N/A", file_or_dir, datetime.now(timezone.utc))
-        elif os.path.isdir(file_or_dir):
-                for root, dirs, files in os.walk(file_or_dir):
-                        for file in files:
-                                file_path = os.path.join(root, file)
-                                process_event(None, "N/A", "N/A", file_path, datetime.now(timezone.utc))
-        else:
-                print(f"Invalid path: {file_or_dir}")
+    def process_file(file_path):
+        # Función para procesar cada archivo
+        process_event(None, "N/A", "N/A", file_path, datetime.now(timezone.utc))
 
+    # Crear un pool de 10 hilos
+    with ThreadPoolExecutor(max_workers=THREADS) as executor:
+        if os.path.isfile(file_or_dir):
+            # Si es un archivo, procesarlo directamente
+            executor.submit(process_file, file_or_dir)
+        elif os.path.isdir(file_or_dir):
+            # Si es un directorio, recorrer los archivos y enviar cada uno a un hilo
+            for root, dirs, files in os.walk(file_or_dir):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    executor.submit(process_file, file_path)
+        else:
+            print(f"Ruta no válida: {file_or_dir}")
 # Process Suricata logs
 def process_suricata_log(suricata_log_file):
         with open(suricata_log_file, 'r') as log_file:
@@ -112,7 +125,7 @@ def process_suricata_log(suricata_log_file):
 def process_zeek_log(zeek_log_file):
         # Run 'zeek-cut' command using subprocess
         command = f"cat {zeek_log_file} | zeek-cut id.orig_h id.resp_h extracted"
-        
+
         # Execute the command in the shell and capture the output
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
@@ -120,7 +133,7 @@ def process_zeek_log(zeek_log_file):
         if process.returncode != 0:
                 print(f"Error in executing zeek-cut: {stderr.decode()}")
                 return
-        
+
         # Process the output of the command
         for line in stdout.decode().splitlines():
                 parts = line.split()
@@ -128,25 +141,25 @@ def process_zeek_log(zeek_log_file):
                         src_ip = parts[0]  # id.orig_h
                         dest_ip = parts[1]  # id.resp_h
                         filename = parts[2]  # extracted
-                        print(f"src: {src_ip}, dst: {dest_ip}, path: {filename}") 
+                        print(f"src: {src_ip}, dst: {dest_ip}, path: {filename}")
                         event_timestamp = datetime.now(timezone.utc)
                         process_event({}, src_ip, dest_ip, filename, event_timestamp)
 
 def process_event(eve, src_ip, dest_ip, filename, event_timestamp):
         if eve is not None and args.suricata_log_file:
                 name = eve['fileinfo']['sha256'] if 'fileinfo' in eve else 'unknown'
-        elif args.zeek_log_file:  
-                name = filename  
+        elif args.zeek_log_file:
+                name = filename
                 # Run 'find' to search the file in filestore_path
 
-        if args.file_or_dir is None:    
+        if args.file_or_dir is None:
                 find_command = ['find', args.filestore_path, '-name', name]
                 try:
                         # Capture the result of 'find' and strip any newlines
                         captured_file = subprocess.run(
                                 find_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
                         ).stdout.strip()
-                        
+
                         # If the file is not found, capture a warning message
                         if not captured_file:
                                 return  # Exit the function if the file does not exist
@@ -158,19 +171,28 @@ def process_event(eve, src_ip, dest_ip, filename, event_timestamp):
                 captured_file = filename  # Use directly the name if it's not a Suricata event
 
         # Run the file scan with ClamAV
-        scan_result = subprocess.run(
+        clamav_result = subprocess.run(
                 ['clamdscan', '--no-summary', '--stdout', '--stream', captured_file],
+                stdout=subprocess.PIPE, text=True
+        ).stdout.strip()
+        yara_result = subprocess.run(
+                ['yara', '-r', YARA_RULES_PATH, captured_file],
                 stdout=subprocess.PIPE, text=True
         ).stdout.strip()
 
         # Generate the log message with the event details
-        log_message = (f"Generated by virus-scanner at timestamp={event_timestamp} "
+        clamav_message = (f"Generated by ClamAV at timestamp={event_timestamp} "
                                    f"src_ip={src_ip} dest_ip={dest_ip} "
-                                   f"filename={filename} scan={scan_result}")
+                                   f"filename={filename} scan={clamav_result}")
+        yara_message = (f"Generated by YARA at timestamp={event_timestamp} "
+                                   f"src_ip={src_ip} dest_ip={dest_ip} "
+                                   f"filename={filename} scan={yara_result}")
 
         # If a threat is detected (FOUND), log and display the message
-        if re.search(r"FOUND\b", scan_result):
-                logger.info(log_message)
+        if re.search(r"FOUND\b", clamav_result):
+                logger.info(clamav_message)
+        if yara_result != "":
+                logger.info(yara_message)
 
         # Save the date of the last processed event if it's a Suricata event
         if eve is not None:
@@ -178,8 +200,6 @@ def process_event(eve, src_ip, dest_ip, filename, event_timestamp):
 
 # Run log processing based on arguments
 process_logs()
-
-
 
 
 
